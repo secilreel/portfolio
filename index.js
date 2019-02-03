8f774b16d6206d535355e15c6bb59af9b4c2d024
Irem Sen <secilreel@yahoo.com>
2019-02-02 21:19:06 -0500

    working on getting technologies logos

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 /* global $ */
 'use strict';
 
-function handleNavigationAboutMe() {
-  $('.navigation-items').on('click', function(e){
-    e.preventDefault();
-    console.log('navigation bar clicked');
-    $('.content').append(aboutMeHTML());
-  });
-}
+// function handleNavigationAboutMe() {
+//   $('.navigation-items').on('click', function(e){
+//     e.preventDefault();
+//     console.log('navigation bar clicked');
+//     $('.content').append(aboutMeHTML());
+//   });
+// }
 
-function aboutMeHTML(){
-  return `
-    <section class="bio-info">
-        <h2>Bio</h2>
-        <p>Hi, this is Irem Secil Reel Sen, a fullstack developer with a mindset on the possibilities of user stories. I go by seçil -- read as say-chill, sounds similar to Rachel. I am eager to grow in coding under great programmers.</p> 
-        <p>If you catch me offline, I might be writing my first book or dancing professionally <a href="https://www.youtube.com/watch?v=s4CT8145GZk" alt="ethnic dance troupe" target="_blank">here</a>.</p>
-    </section>
-    <a href="mailto: secilreel@yahoo.com" target="blank">Contact me via email</a>
-    <a href="https://github.com/secilreel" alt="github account link" target="_blank">
-        <img src="resources/github.svg" class="footer-logo" alt="github logo">
-    </a>
-    <a href="https://www.linkedin.com/in/irem-secil-reel-sen/" alt="linkedin account link" target="_blank">
-        <img src="resources/linkedin.svg" class="footer-logo" alt="linkedin logo">
-    </a>`;
-}
+// function aboutMeHTML(){
+//   return `
+//     <section class="bio-info">
+//         <h2>Bio</h2>
+//         <p>Hi, this is Irem Secil Reel Sen, a fullstack developer with a mindset on the possibilities of user stories. I go by seçil -- read as say-chill, sounds similar to Rachel. I am eager to grow in coding under great programmers.</p> 
+//         <p>If you catch me offline, I might be writing my first book or dancing professionally <a href="https://www.youtube.com/watch?v=s4CT8145GZk" alt="ethnic dance troupe" target="_blank">here</a>.</p>
+//     </section>
+//     <a href="mailto: secilreel@yahoo.com" target="blank">Contact me via email</a>
+//     <a href="https://github.com/secilreel" alt="github account link" target="_blank">
+//         <img src="resources/github.svg" class="footer-logo" alt="github logo">
+//     </a>
+//     <a href="https://www.linkedin.com/in/irem-secil-reel-sen/" alt="linkedin account link" target="_blank">
+//         <img src="resources/linkedin.svg" class="footer-logo" alt="linkedin logo">
+//     </a>`;
+// }
 
 
 $(handleNavigationAboutMe);
